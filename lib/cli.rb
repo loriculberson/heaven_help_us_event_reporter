657b6bd667a0a3e669e4b3a6416aa549d1a29c27
Brandon Mrsny <bmrsny@gmail.com>
2014-11-22 16:08:03 -0700

    added basic Repl fucntions

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,10 +10,35 @@ class Cli
 
   def call
     puts "hello"
-    process_commands
+    until quit?
+      user_input
+      process_commands
+    end
+    puts "you got here"
   end
 
   def process_commands
+     case
+     when load?
+      puts "You got some loaded files"
+    when find?
+      puts "You found some files"
+     end
+  end
 
+  def quit?
+    @command == "quit" || @command == "q"
+  end
+
+  def load?
+    @command == "load"
+  end
+
+  def find?
+    @command == "find"
+  end
+
+  def user_input
+    @command = gets.strip.downcase
   end
 end
